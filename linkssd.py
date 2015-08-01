1a744729a39ee98c9d7916a2240ce21ec46c1dcf
Keith Batten <kbatten@gmail.com>
2015-08-01 12:59:28 -0700

    refactor so it is easier to add junctions

--- linkssd.py
+++ linkssd.py
@@ -1,39 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import time
 import os
 import sys
 
-try:
-    from ntfsutils.junction import isjunction
-    from ntfsutils.junction import create as createjunction
-    from ntfsutils.junction import unlink as unlinkjunction
-    _junction_data = {}
-except ImportError:
-    _junction_data = {}
-    def isjunction(directory):
-        if directory in _junction_data:
-            return True
-        return False
-
-    def createjunction(directory, junction):
-        if junction in _junction_data:
-            raise Exception("%s: junction link name already exists" % junction)
-        _junction_data[junction] = directory
-
-    def unlinkjunction(path):
-        if not isjunction(path):
-            raise Exception("%s does not exist or is not a junction" % path)
-        _junction_data[path]
-
-    def walk(base):
-        yield [base, ['dir1','dir2','dir3'], None]
-    def join(base,d):
-        return base + '\\' + d
-    os.walk = walk
-    os.path.join = join
-
-    createjunction(r"E:\SSD\torrents", os.path.expanduser("~") + r"\Downloads\torrents")
+import ntfsutils.junction
 
 
 def list_directories(base):
@@ -43,76 +16,46 @@ def list_directories(base):
         directories = []
     return directories
 
-
-junction_data = {}
-home = os.path.expanduser("~")
-
-# Link spinning disk steam common files
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Steam\steamapps\common"):
-        directory = drive + r":\SSD\Steam\steamapps\common" + "\\" + d
-        junction = r"C:\Program Files (x86)\Steam\steamapps\common" + "\\" + d
-        junction_data[junction] = directory
-
-# Link spinning disk steam sourcemods
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Steam\steamapps\sourcemods"):
-        directory = drive + r":\SSD\Steam\steamapps\sourcemods" + "\\" + d
-        junction = r"C:\Program Files (x86)\Steam\steamapps\sourcemods" + "\\" + d
-        junction_data[junction] = directory
-
-# Link spinning disk torrent downloads
-junction_data[home + r"\Downloads\torrents"] = r"E:\SSD\torrents"
-
-# Link spinning disk itunes music
-junction_data[home + r"\Music\iTunes"] = r"E:\SSD\iTunes_music"
-
-# Link spinning disk apple mobile backup
-junction_data[home + r"\AppData\Roaming\Apple Computer\MobileSync\Backup"] = r"E:\SSD\Apple_mobile_backup"
-
-# Link AppData\Roaming
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Roaming"):
-        directory = drive + r":\SSD\Roaming" + "\\" + d
-        junction = home + r"\AppData\Roaming" + "\\" + d
-        junction_data[junction] = directory
-
-# Link Origin Games
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Origin Games"):
-        directory = drive + r":\SSD\Origin Games" + "\\" + d
-        junction = r"C:\Program Files (x86)\Origin Games" + "\\" + d
-        junction_data[junction] = directory
-
-# Link Program Files (x86)
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Program Files (x86)"):
-        directory = drive + r":\SSD\Program Files (x86)" + "\\" + d
-        junction = r"C:\Program Files (x86)" + "\\" + d
-        junction_data[junction] = directory
-
-# Link Program Files
-for drive in ['D', 'E']:
-    for d in list_directories(drive + r":\SSD\Program Files"):
-        directory = drive + r":\SSD\Program Files" + "\\" + d
-        junction = r"C:\Program Files" + "\\" + d
-        junction_data[junction] = directory
-
-#######################
-
-for junction in junction_data.keys():
-    directory = junction_data[junction]
+def create_junction(directory, junction):
     try:
-        unlinkjunction(junction)
+        ntfsutils.junction.unlink(junction)
     except Exception as e:
         if str(e).strip().endswith(' does not exist or is not a junction'):
             pass
         else:
             raise e
     try:
-        createjunction(directory, junction)
+        ntfsutils.junction.create(directory, junction)
     except Exception as e:
         if str(e).strip().endswith(': junction link name already exists'):
             pass
         else:
             raise e
+
+def create_junction_all(directory, junction):
+    for drive in drives:
+        for d in list_directories(drive + directory):
+            directory_fqn = drive + directory + "\\" + d
+            create_junction(directory_fqn, junction + "\\" + d)
+
+
+drives = ['D', 'E']
+home = os.path.expanduser("~")
+
+
+def main():
+    create_junction_all(r":\SSD\Steam\steamapps\common",
+                        r"C:\Program Files (x86)\Steam\steamapps\common")
+    create_junction_all(r":\SSD\Steam\steamapps\sourcemods",
+                        r"C:\Program Files (x86)\Steam\steamapps\sourcemods")
+    create_junction(r"E:\SSD\torrents", home + r"\Downloads\torrents")
+    #create_junction(r"E:\SSD\iTunes_music", home + r"\Music\iTunes")
+    #create_junction(r"E:\SSD\Apple_mobile_backup", home + r"\AppData\Roaming\Apple Computer\MobileSync\Backup")
+    create_junction_all(r":\SSD\Roaming", home + r"\AppData\Roaming")
+    create_junction_all(r":\SSD\Origin Games", r"C:\Program Files (x86)\Origin Games")
+    create_junction_all(r":\SSD\Program Files (x86)", r"C:\Program Files (x86)")
+    create_junction_all(r":\SSD\Program Files", r"C:\Program Files")
+
+
+if __name__ == "__main__":
+    main()
